6d766ec90d6c519c5d2124254b5c33b8b51a3da7
Bastien Nocera <hadess@hadess.net>
2020-12-15 11:09:54 +0100

    remove duplicated license

--- src/udev/dmi_memory_id/dmi_memory_id.c
+++ src/udev/dmi_memory_id/dmi_memory_id.c
@@ -6,26 +6,6 @@
  *   Copyright (C) 2002-2020 Jean Delvare <jdelvare@suse.de>
  *   Copyright (C) 2020 Bastien Nocera <hadess@hadess.net>
  *
- *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 2 of the License, or
- *   (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program; if not, write to the Free Software
- *   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA
- *
- *   For the avoidance of doubt the "preferred form" of this code is one which
- *   is in an open unpatent encumbered format. Where cryptographic key signing
- *   forms part of the process of creating an executable the information
- *   including keys needed to generate an equivalently functional executable
- *   are deemed to be part of the source code.
- *
  * Unless specified otherwise, all references are aimed at the "System
  * Management BIOS Reference Specification, Version 3.2.0" document,
  * available from http://www.dmtf.org/standards/smbios.
